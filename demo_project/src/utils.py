ac7fa5c234c6fa96610f88aa3528ea95b7cc26b0
Shreyans06 <shreyanskjain06@gmail.com>
2025-01-22 16:29:13 -0800

    Added prediction pipeline and elastic bean stalk settings

--- demo_project/src/utils.py
+++ demo_project/src/utils.py
@@ -6,6 +6,7 @@ import pandas as pd
 import dill
 from src.exception import CustomException
 from sklearn.metrics import mean_squared_error , r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path , obj):
     '''
@@ -21,14 +22,22 @@ def save_object(file_path , obj):
     except Exception as e:
         raise CustomException(e , sys)
 
-def evaluate_model(X_train , y_train , X_test , y_test , models):
+def evaluate_model(X_train , y_train , X_test , y_test , models , param ):
     '''
     This function is used to evaluate the model
     '''
     try:
         model_report = {}
         for model_name , model in models.items():
+            # model.fit(X_train , y_train)
+            params = param[model_name]
+
+            gs = GridSearchCV(model , params , cv = 5 , n_jobs = -1 , verbose = 1)
+            gs.fit(X_train , y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train , y_train)
+
             y_pred = model.predict(X_test)
             y_train_pred = model.predict(X_train)
 
@@ -40,6 +49,14 @@ def evaluate_model(X_train , y_train , X_test , y_test , models):
             }
 
         return model_report
+    
+    except Exception as e:
+        raise CustomException(e , sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path , "rb") as file_obj:
+            return dill.load(file_obj)
     
     except Exception as e:
         raise CustomException(e , sys)
